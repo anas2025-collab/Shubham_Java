b26ec3446eeb570b9b2f5fac1ecb56e0b68496b4
anas2025-collab <sheikhanas2023@gmail.com>
2025-02-05 20:52:29 +0530

    first commmit

---
+++ employeemanagement/src/main/java/com/example/employeemanagement/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.example.employeemanagement;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long id, Employee employee) {
+        employee.setId(id);
+        return employeeRepository.save(employee);
+    }
+    
+
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
